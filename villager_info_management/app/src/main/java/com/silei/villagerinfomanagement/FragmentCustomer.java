f7383c8d1de68b28c38487d1219b43836980b450
sileixinhua <sileixinhua@163.com>
2021-08-22 21:55:48 +0800

    【Task】make fragment_customer UI ready, add CircleImageView, add a lots of icons, add a lots of xml resource.

--- villager_info_management/app/src/main/java/com/silei/villagerinfomanagement/FragmentCustomer.java
+++ villager_info_management/app/src/main/java/com/silei/villagerinfomanagement/FragmentCustomer.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
 public class FragmentCustomer extends Fragment {
     private static final String TAG = FragmentCustomer.class.getSimpleName();
 
@@ -25,7 +24,7 @@ public class FragmentCustomer extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_customer, container, false);
-        TextView textView = (TextView) view.findViewById(R.id.textView);
+        // TextView textView = (TextView) view.findViewById(R.id.textView);
         return view;
     }
 }
